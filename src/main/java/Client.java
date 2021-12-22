8eadeb6a5af67adbe0bef813fa66c470dc1552ca
uukkii <i.am.newbie@hotmail.com>
2021-12-22 13:40:42 +0300

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        String host = "127.0.0.1";
+        int port = 8083;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Netology\n");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+    }
+}
